064015c4079d7dde4af8acb34d57ed7f179a7ab0
A-nita <nimoura1@gmail.com>
2024-03-19 23:37:13 -0300

    :tada: Feat: Starting the Project

--- seapunk-web/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Background from "@/components/background/Background";
-
-export default function Home() {
-  return (
-    <main className="flex  min-h-screen flex-col items-start justify-between p-5 bg-slate-950">
-      <Background />
-    </main>
-  );
-}
